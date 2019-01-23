4e7458a2156df76b0a8b2347570574cf9566a4ad
terfno <sueda.takahito@gmail.com>
2019-01-23 12:31:18 +0900

    add unity project file

---
+++ webGLpractice/Assets/button.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+	public void OnClick(){
+		Debug.Log("Button Click!");
+        Vector3 tmp = GameObject.Find("tama").transform.position;
+        GameObject.Find("tama").transform.position = new Vector3(tmp.x, tmp.y = 3.42f, tmp.z);
+    }
+}
